830a3437a33c69ae2c3e296e63903ae84da746e7
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-09-28 14:30:04 -0700

    more test, bikin sendiri
    more test, bikin sendiri

--- Ethereum and Solidity (Developer Guide)/lottery/test/Lottery.test.js
+++ Ethereum and Solidity (Developer Guide)/lottery/test/Lottery.test.js
@@ -20,84 +20,80 @@ describe("Lottery Contract", () => {
     assert.ok(lottery.options.address);
   });
 
-  it('allows one account to enter', async () => {
+  it("allows one account to enter", async () => {
     await lottery.methods.enter().send({
-        from: accounts[0],
-        value: web3.utils.toWei('0.02', 'ether') //convert ke wei
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"), //convert ke wei
     });
 
     const players = await lottery.methods.getPlayers().call({
-        from : accounts[0]
+      from: accounts[0],
     });
 
     assert.equal(accounts[0], players[0]);
     assert.equal(1, players.length);
-
   });
 
-  it('allows multiple account to enter', async () => {
+  it("allows multiple account to enter", async () => {
     await lottery.methods.enter().send({
-        from: accounts[0],
-        value: web3.utils.toWei('0.02', 'ether') //convert ke wei
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"), //convert ke wei
     });
 
     await lottery.methods.enter().send({
-        from: accounts[1],
-        value: web3.utils.toWei('0.02', 'ether') //convert ke wei
+      from: accounts[1],
+      value: web3.utils.toWei("0.02", "ether"), //convert ke wei
     });
 
     await lottery.methods.enter().send({
-        from: accounts[2],
-        value: web3.utils.toWei('0.02', 'ether') //convert ke wei
+      from: accounts[2],
+      value: web3.utils.toWei("0.02", "ether"), //convert ke wei
     });
 
     const players = await lottery.methods.getPlayers().call({
-        from : accounts[0]
+      from: accounts[0],
     });
 
     assert.equal(accounts[0], players[0]);
     assert.equal(accounts[1], players[1]);
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length);
-
-  });
-  
-  it('requires a minimum amount of ether to enter', async () => {
-
-    try{
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: 200
-        });
-        assert(false); // kalo trynya gk ada error, maka ini akan jalan, dan testnya gagal
-    } catch (err) {
-        // Karena kita udah kasih validasi di contractnya, kalo error berarti testnya berhasil karena kita kirim < 0.1 ether. assert() ngecek truthiness, kalo errornya ada jadi true, jadi pass the test
-        assert(err);        
-    }
   });
 
-  it('only manager can call pickWinner', async () => {
-    try{
-        await lottery.methods.pickWinner().send({
-            from: accounts[1]
-        });
-        assert(false)
-    } catch (err) {
-        assert(err);
-    }
-  });
-
-  it('sends money to the winner and resets the players array', async () => {
-
-    await lottery.methods.enter().send({
+  it("requires a minimum amount of ether to enter", async () => {
+    try {
+      await lottery.methods.enter().send({
         from: accounts[0],
-        value: web3.utils.toWei('2', 'ether')
+        value: 200,
+      });
+      assert(false); // kalo trynya gk ada error, maka ini akan jalan, dan testnya gagal
+    } catch (err) {
+      // Karena kita udah kasih validasi di contractnya, kalo error berarti testnya berhasil karena kita kirim < 0.1 ether. assert() ngecek truthiness, kalo errornya ada jadi true, jadi pass the test
+      assert(err);
+    }
+  });
+
+  it("only manager can call pickWinner", async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("sends money to the winner and resets the players array", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("2", "ether"),
     });
 
     const initialBalance = await web3.eth.getBalance(accounts[0]);
 
     await lottery.methods.pickWinner().send({
-        from: accounts[0]
+      from: accounts[0],
     });
 
     const finalBalance = await web3.eth.getBalance(accounts[0]);
@@ -107,20 +103,33 @@ describe("Lottery Contract", () => {
     // assert(difference > web3.utils.toWei('1.8', 'ether'));
 
     assert(finalBalance > initialBalance); // kalo lebih besar berarti duitnya dah balik
-
   });
 
+  it("players are emptied", async () => {
+    await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei("0.02", "ether"), //convert ke wei
+      });
+  
+      await lottery.methods.enter().send({
+        from: accounts[1],
+        value: web3.utils.toWei("0.02", "ether"), //convert ke wei
+      });
+  
+      await lottery.methods.enter().send({
+        from: accounts[2],
+        value: web3.utils.toWei("0.02", "ether"), //convert ke wei
+      });
+
+      await lottery.methods.pickWinner().send({
+        from: accounts[0],
+      });
+
+      const players_now = await lottery.methods.getPlayers().call({
+        from: accounts[0],
+      });
+
+      assert.equal(players_now.length, 0);
+  })
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
