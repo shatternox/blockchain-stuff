9aa63a097f56b32ac16c26de98731ccece0d2101
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-10-05 16:30:54 +0700

    lottery updatelottery update
    lottery updatelottery update

---
+++ Ethereum and Solidity (Developer Guide)/lottery-react/src/lottery.js
@@ -0,0 +1,72 @@
+import web3 from "./web3";
+
+const address = "0x2F7c37BC677DeAbF9E2D078c8C4D701b39d01c52";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
